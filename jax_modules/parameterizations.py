088633253b0c59142c93cdd1a1c68be7bcc08a53
carlosgmartin <cgmartin@andrew.cmu.edu>
2025-05-29 00:43:37 -0400

    Add Ball parameterization module.

--- jax_modules/parameterizations.py
+++ jax_modules/parameterizations.py
@@ -5,6 +5,27 @@ from jax import numpy as jnp
 from .module import Module
 
 
+class Ball(Module):
+    def __init__(
+        self,
+        dim,
+        initializer=nn.initializers.zeros,
+        regularizer=lambda param: 0.0,
+    ):
+        self.dim = dim
+        self.initializer = initializer
+        self.regularizer = regularizer
+
+    def init(self, key):
+        return self.initializer(key, (self.dim,))
+
+    def apply(self, param):
+        return param / jnp.sqrt(1 + jnp.square(param))
+
+    def param_loss(self, param):
+        return self.regularizer(param)
+
+
 def vector_to_symmetric_matrix(vector, dim):
     i = jnp.triu_indices(dim)
     A = jnp.zeros([dim, dim], vector.dtype)

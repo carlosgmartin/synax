f012cc176f973f4fd7c98f31ad4c1436d81f52d6
carlosgmartin <cgmartin@andrew.cmu.edu>
2025-05-29 16:52:30 -0400

    Add docstring for layer_norm.

--- jax_modules/utils.py
+++ jax_modules/utils.py
@@ -2,6 +2,8 @@ from jax import numpy as jnp
 
 
 def layer_norm(x, axis=-1, epsilon=1e-6):
+    """Layer normalization
+    https://arxiv.org/abs/1607.06450"""
     x -= x.mean(axis, keepdims=True)
     x /= jnp.sqrt((x * jnp.conj(x)).mean(axis, keepdims=True) + epsilon)
     return x

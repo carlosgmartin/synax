9f2c03040533396e3869ad47039b85c4800da911
carlosgmartin <cgmartin@andrew.cmu.edu>
2025-05-29 16:41:41 -0400

    Add regularizers module.

---
+++ jax_modules/regularizers.py
@@ -0,0 +1,39 @@
+from jax import numpy as jnp
+
+
+def zero(x):
+    return 0.0
+
+
+def l1(x):
+    return jnp.abs(x).sum()
+
+
+def l2(x, squared=False):
+    x = (x * jnp.conj(x)).sum()
+    if squared:
+        return x
+    else:
+        return x
+
+
+def linf(x):
+    return jnp.abs(x).max()
+
+
+def lp(x, p):
+    return (jnp.abs(x) ** p).sum() ** (1 / p)
+
+
+def scale(regularizer, scale):
+    def f(x):
+        return regularizer(x) * scale
+
+    return f
+
+
+def add(regularizers):
+    def f(x):
+        return sum(regularizer(x) for regularizer in regularizers)
+
+    return f

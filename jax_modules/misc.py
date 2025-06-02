077222218c193cc7934a8e6da59ed3f1c891e2dc
carlosgmartin <cgmartin@andrew.cmu.edu>
2025-06-02 17:24:39 -0400

    Use MinimalGatedUnit with reset_gate=False by default in NeuralCellularAutomaton.

--- jax_modules/misc.py
+++ jax_modules/misc.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from jax import nn, random
 from jax import numpy as jnp
 
@@ -111,7 +113,12 @@ class NeuralCellularAutomaton(Module):
     """Neural GPUs learn algorithms (2015)
     https://arxiv.org/abs/1511.08228"""
 
-    def __init__(self, state_dim, space_dim=1, cell_cls=MinimalGatedUnit):
+    def __init__(
+        self,
+        state_dim,
+        space_dim=1,
+        cell_cls=partial(MinimalGatedUnit, reset_gate=False),
+    ):
         self.cell = cell_cls(state_dim, state_dim * 2 * space_dim)
 
     def init(self, key):

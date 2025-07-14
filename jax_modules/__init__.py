e6cc782b239c995a10374a9c67c9cae5d518c2cb
carlosgmartin <cgmartin@andrew.cmu.edu>
2025-07-13 20:07:08 -0400

    Reorder imports.

--- jax_modules/__init__.py
+++ jax_modules/__init__.py
@@ -7,41 +7,36 @@ from ._basic import Func as Func
 from ._basic import Scale as Scale
 from ._compound import Chain as Chain
 from ._compound import Parallel as Parallel
+from ._misc import GLU as GLU
+from ._misc import MLP as MLP
+from ._misc import AlexNet as AlexNet
+from ._misc import AutoEncoder as AutoEncoder
+from ._misc import LeNet as LeNet
+from ._misc import PReLU as PReLU
+from ._recurrent import GRU as GRU
+from ._recurrent import LSTM as LSTM
+from ._recurrent import MGU as MGU
+from ._recurrent import SimpleRNN as SimpleRNN
+from ._utils import layer_norm as layer_norm
+from ._utils import max_pool as max_pool
+from ._utils import mean_pool as mean_pool
+from ._utils import rms_norm as rms_norm
+from ._utils import sum_pool as sum_pool
 
 # from ._compound import ConvexPotentialFlow as ConvexPotentialFlow
 # from ._compound import RavelUnravel as RavelUnravel
 # from ._compound import Repeat as Repeat
 # from ._compound import Residual as Residual
 # from ._compound import Switch as Switch
-from ._misc import GLU as GLU
-from ._misc import MLP as MLP
-from ._misc import AlexNet as AlexNet
-from ._misc import AutoEncoder as AutoEncoder
-from ._misc import LeNet as LeNet
-
 # from ._misc import NeuralCellularAutomaton as NeuralCellularAutomaton
-from ._misc import PReLU as PReLU
-from ._module import Module as Module
-
 # from ._parameterizations import AntisymmetricMatrix as AntisymmetricMatrix
 # from ._parameterizations import Ball as Ball
 # from ._parameterizations import Constant as Constant
 # from ._parameterizations import Simplex as Simplex
 # from ._parameterizations import SpecialOrthogonalMatrix as SpecialOrthogonalMatrix
 # from ._parameterizations import SymmetricMatrix as SymmetricMatrix
-from ._recurrent import GRU as GRU
-from ._recurrent import LSTM as LSTM
-from ._recurrent import MGU as MGU
-
 # from ._recurrent import BistableRecurrentCell as BistableRecurrentCell
 # from ._recurrent import ConvolutionalGatedUnit as ConvolutionalGatedUnit
 # from ._recurrent import FastGRNN as FastGRNN
 # from ._recurrent import RecurrentNetwork as RecurrentNetwork
-from ._recurrent import SimpleRNN as SimpleRNN
-
 # from ._recurrent import UpdateGateRNN as UpdateGateRNN
-from ._utils import layer_norm as layer_norm
-from ._utils import max_pool as max_pool
-from ._utils import mean_pool as mean_pool
-from ._utils import rms_norm as rms_norm
-from ._utils import sum_pool as sum_pool
